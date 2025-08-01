c532ff24c85fdadd1c3aedc68b3b771addb1c5dc
atifformateur <atif.zourgani@laplateforme.io>
2025-08-01 11:23:53 +0200

    fin de l'exo ( a corriger l'affichage en get)

--- public/index.php
+++ public/index.php
@@ -7,21 +7,22 @@
     }
 
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        
-        $name = $_POST["name"] ?? '';
-        $email = $_POST["email"] ?? '';
-        $message = $_POST["message"] ?? '';
+
+        $name = htmlspecialchars(trim($_POST["name"] ?? ''));
+        $email = htmlspecialchars(trim($_POST["email"] ?? ''));
+        $message = htmlspecialchars(trim($_POST["message"] ?? ''));
 
         if(!empty($name) && !empty($email) && !empty($message)) {
+
             $newContact = [
                 "name" => $name,
                 "email" => $email,
                 "message" => $message
             ];
 
-            //nettoyer les données
-            
-            //enregistrer les données (dans cet exemple c'est dans la session en attendant de voir la bdd)
+            $_SESSION['contacts'][] = $newContact;
+
+            $contacts = $_SESSION['contacts'];
 
         }
     }
@@ -33,15 +34,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../asset/css/style.css">
 </head>
 <body>
+    <h1>Formulaire de contact</h1>
      <main>
-        <form action="" method="POST">
-            <input placeholder="name" type="text" id="name" name="name">
-            <input placeholder="email" type="email" id="email" name="email">
-            <input placeholder="message" type="message" id="message" name="message">
-            <input type="submit" value="Envoyer">
-        </form>
+        <section>
+            <h2>Le formulaire</h2>
+            <form class="formContainer" action="" method="POST">
+                <div>
+                    <input placeholder="name" type="text" id="name" name="name">
+                </div>
+                <div>
+                    <input placeholder="email" type="email" id="email" name="email">
+                </div>
+                <div>
+                    <input placeholder="message" type="message" id="message" name="message">
+                </div>
+                <div>
+                    <input type="submit" value="Envoyer">
+                </div>
+            </form>
+        </section>
+        <section>
+            <h2>Nos messages reçu</h2>
+            <div class="contactContainer">
+                <?php 
+                    if(!empty($contacts)) {
+                        foreach ($contacts as $contact) {
+                            echo "
+                            <article>
+                                <h3>$contact[name]</h3>
+                                <span>l'email est: $contact[email]</span>
+                                <p>
+                                    $contact[message]
+                                </p>
+                            </article>
+                            ";
+                        }
+                        
+                    }else{
+                        echo "
+                            <p>
+                                Nous n'avons pas reçu de messages!
+                            </p>
+                        ";
+                    }
+                ?>
+            </div>
+        </section>
     </main>
 </body>
 </html>
